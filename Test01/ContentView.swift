e0d062fa86da47f31ced639c38990e1e08195242
blgn <btezeren@yahoo.com>
2020-09-28 21:19:14 +0300

    Test commit

---
+++ Test01/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Test01
+//
+//  Created by abt on 26.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Text("Test01")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test01/Test01App.swift
@@ -0,0 +1,23 @@
+//
+//  Test01App.swift
+//  Test01
+//
+//  Created by abt on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct Test01App_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
